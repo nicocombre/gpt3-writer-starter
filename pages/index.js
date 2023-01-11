32e7a4f99368712225cf51c9936d71682789e698
nicocombre <93434064+nicocombre@users.noreply.github.com>
2023-01-11 06:08:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 const Home = () => {
   const [userInput, setUserInput] = useState('');
   const onUserChangedText = (event) => {
-    console.log(event.target.value);
     setUserInput(event.target.value);
   };
   return (

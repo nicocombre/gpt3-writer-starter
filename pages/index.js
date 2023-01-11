a7a213d1298299765d4dc02598338f26c1f31b30
nicocombre <93434064+nicocombre@users.noreply.github.com>
2023-01-11 06:32:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,10 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>B2B Sales prospection assistant</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Input your message or email, you will know what is next </h2>
           </div>
         </div>
         <div className="prompt-container">

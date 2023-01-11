24f602da2a6fe20be9d90148e8b1b5e3e500bc90
nicocombre <93434064+nicocombre@users.noreply.github.com>
2023-01-11 16:20:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,28 @@ import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
   const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
@@ -30,7 +52,7 @@ const Home = () => {
             onChange={onUserChangedText}
           />
                <div className="prompt-buttons">
-    <a className="generate-button" onClick={null}>
+    <a className="generate-button" onClick={callGenerateEndpoint}>
       <div className="generate">
         <p>Generate</p>
       </div>
